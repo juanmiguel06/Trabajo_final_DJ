ce55c51d4e5fd4a5cc6b367a61a4b9cb7755b4ed
juanmiguel06 <Juanmilopezq08@gmail.com>
2023-11-27 16:24:30 -0500

    bien hecho

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\juanm\OneDrive\Escritorio\Project_Base_dj\venv
+set VIRTUAL_ENV=C:\Users\juanm\OneDrive\Escritorio\Trabajo_final_DJ\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
